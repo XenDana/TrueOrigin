f3d07299c41858004ce6dd15db0659f29ae1a69a
Alvin Natawiguna <alvin.nt.gg@gmail.com>
2024-08-17 02:01:41 +0700

    feat: implement mock struct for organization

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,4 +1,43 @@
-#[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+use candid::{CandidType, Nat, Principal, Deserialize};
+use ic_cdk::{init, query, update};
+
+#[derive(CandidType, Deserialize)]
+struct Metadata {
+    key: String,
+    value: String,
+}
+
+#[derive(CandidType, Deserialize)]
+struct Organization {
+    id: Principal,
+    name: String,
+    description: String,
+    metadata: Vec<Metadata>,
+    created_at: Nat,
+    created_by: Principal,
+    updated_at: Nat,
+    updated_by: Principal,
+}
+
+#[derive(CandidType, Deserialize)]
+struct OrganizationInput {
+    name: String,
+    description: String,
+    metadata: Vec<Metadata>,
+}
+
+
+#[query]
+fn getOrganizationById(id: Principal) -> Option<Organization> {
+    Err("Not Implemented")
+}
+
+#[update]
+fn createOrganization(input: OrganizationInput) -> Organization {
+    Err("Not Implemented")
+}
+
+#[update]
+fn updateOrganization(id: Principal, input: OrganizationInput) -> Organization {
+    Err("Not Implemented")
 }
