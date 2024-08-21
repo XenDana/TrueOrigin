ad2b349964c7d93a12d89339e964284bfed4889d
Alvin Natawiguna <alvin.nt.gg@gmail.com>
2024-08-22 00:29:11 +0800

    feat: add internet identity to FE

--- src/declarations/TrustOrigin_backend/TrustOrigin_backend.did.d.ts
+++ src/declarations/TrustOrigin_backend/TrustOrigin_backend.did.d.ts
@@ -37,17 +37,46 @@ export interface ProductInput {
   'description' : string,
   'category' : string,
 }
+export interface User {
+  'id' : Principal,
+  'updated_at' : bigint,
+  'updated_by' : Principal,
+  'org_ids' : Array<Principal>,
+  'is_principal' : boolean,
+  'is_enabled' : boolean,
+  'created_at' : bigint,
+  'created_by' : Principal,
+  'email' : [] | [string],
+  'first_name' : [] | [string],
+  'detail_meta' : Array<Metadata>,
+  'last_name' : [] | [string],
+  'phone_no' : [] | [string],
+}
+export interface UserDetailsInput {
+  'email' : string,
+  'first_name' : string,
+  'detail_meta' : Array<Metadata>,
+  'last_name' : string,
+  'phone_no' : string,
+}
 export interface _SERVICE {
   'create_organization' : ActorMethod<[OrganizationInput], Organization>,
   'create_product' : ActorMethod<[ProductInput], Product>,
+  'create_user' : ActorMethod<[Principal, UserDetailsInput], User>,
   'get_organization_by_id' : ActorMethod<[Principal], [] | [Organization]>,
   'get_product_by_id' : ActorMethod<[Principal], [] | [Product]>,
+  'get_user_by_id' : ActorMethod<[Principal], [] | [User]>,
   'greet' : ActorMethod<[string], string>,
+  'register' : ActorMethod<[], User>,
   'update_organization' : ActorMethod<
     [Principal, OrganizationInput],
     Organization
   >,
   'update_product' : ActorMethod<[Principal, ProductInput], Product>,
+  'update_self_details' : ActorMethod<[UserDetailsInput], User>,
+  'update_user' : ActorMethod<[Principal, UserDetailsInput], User>,
+  'update_user_orgs' : ActorMethod<[Principal, Array<Principal>], User>,
+  'whoami' : ActorMethod<[], [] | [User]>,
 }
 export declare const idlFactory: IDL.InterfaceFactory;
 export declare const init: (args: { IDL: typeof IDL }) => IDL.Type[];
--- src/declarations/TrustOrigin_backend/TrustOrigin_backend.did.js
+++ src/declarations/TrustOrigin_backend/TrustOrigin_backend.did.js
@@ -34,9 +34,32 @@ export const idlFactory = ({ IDL }) => {
     'created_by' : IDL.Principal,
     'category' : IDL.Text,
   });
+  const UserDetailsInput = IDL.Record({
+    'email' : IDL.Text,
+    'first_name' : IDL.Text,
+    'detail_meta' : IDL.Vec(Metadata),
+    'last_name' : IDL.Text,
+    'phone_no' : IDL.Text,
+  });
+  const User = IDL.Record({
+    'id' : IDL.Principal,
+    'updated_at' : IDL.Nat64,
+    'updated_by' : IDL.Principal,
+    'org_ids' : IDL.Vec(IDL.Principal),
+    'is_principal' : IDL.Bool,
+    'is_enabled' : IDL.Bool,
+    'created_at' : IDL.Nat64,
+    'created_by' : IDL.Principal,
+    'email' : IDL.Opt(IDL.Text),
+    'first_name' : IDL.Opt(IDL.Text),
+    'detail_meta' : IDL.Vec(Metadata),
+    'last_name' : IDL.Opt(IDL.Text),
+    'phone_no' : IDL.Opt(IDL.Text),
+  });
   return IDL.Service({
     'create_organization' : IDL.Func([OrganizationInput], [Organization], []),
     'create_product' : IDL.Func([ProductInput], [Product], []),
+    'create_user' : IDL.Func([IDL.Principal, UserDetailsInput], [User], []),
     'get_organization_by_id' : IDL.Func(
         [IDL.Principal],
         [IDL.Opt(Organization)],
@@ -47,13 +70,23 @@ export const idlFactory = ({ IDL }) => {
         [IDL.Opt(Product)],
         ['query'],
       ),
+    'get_user_by_id' : IDL.Func([IDL.Principal], [IDL.Opt(User)], ['query']),
     'greet' : IDL.Func([IDL.Text], [IDL.Text], ['query']),
+    'register' : IDL.Func([], [User], []),
     'update_organization' : IDL.Func(
         [IDL.Principal, OrganizationInput],
         [Organization],
         [],
       ),
     'update_product' : IDL.Func([IDL.Principal, ProductInput], [Product], []),
+    'update_self_details' : IDL.Func([UserDetailsInput], [User], []),
+    'update_user' : IDL.Func([IDL.Principal, UserDetailsInput], [User], []),
+    'update_user_orgs' : IDL.Func(
+        [IDL.Principal, IDL.Vec(IDL.Principal)],
+        [User],
+        [],
+      ),
+    'whoami' : IDL.Func([], [IDL.Opt(User)], ['query']),
   });
 };
 export const init = ({ IDL }) => { return []; };
