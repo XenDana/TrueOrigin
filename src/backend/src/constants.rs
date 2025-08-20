437621043efae7d2c8db27065d1f1c535caaa4f4
ayrtoncn <ayrton@xendit.co>
2025-08-21 00:49:47 +0700

    chore: update constants file comments

--- src/backend/src/constants.rs
+++ src/backend/src/constants.rs
@@ -1,5 +1,5 @@
 // Application constants for TrueOrigin backend
 
-// API versioning
+// API versioning constants used throughout the application
 pub const API_VERSION: &str = "1.0";
 pub const CANISTER_VERSION: &str = "1.0.0";
