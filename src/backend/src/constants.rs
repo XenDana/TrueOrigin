124bb0a23ec8858770be46144e5e759f288fe669
ayrtoncn <ayrton@xendit.co>
2025-08-21 00:50:19 +0700

    chore: remove unused constants, keep file for future use

--- src/backend/src/constants.rs
+++ src/backend/src/constants.rs
@@ -1,5 +1,2 @@
 // Application constants for TrueOrigin backend
-
-// API versioning constants used throughout the application
-pub const API_VERSION: &str = "1.0";
-pub const CANISTER_VERSION: &str = "1.0.0";
+// This file is reserved for future application-wide constants
